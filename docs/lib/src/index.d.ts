f44c091052e4733275913525e6bbfc68a9a980b0
Marco Fugaro <marco.fugaro@gmail.com>
2018-12-05 17:41:16 +0100

    🔨 Build docs and lib

---
+++ docs/lib/src/index.d.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './components/Button';
+export { default as Checkbox } from './components/Checkbox';
+export { default as FlexView } from './components/FlexView';
+export { default as Radio } from './components/Radio';
+export { default as Select } from './components/Select';
+export { default as TextInput } from './components/TextInput';
+export { default as Toggle } from './components/Toggle';
---
+++ lib/lib/src/components/Toggle/index.d.ts
@@ -0,0 +1,2 @@
+import { InputPropsTypes } from '../../commons/interfaces';
+export default function Toogle({ children, className, inputClassName, style, disabled, checked, onChange, reset, }: InputPropsTypes): JSX.Element;
