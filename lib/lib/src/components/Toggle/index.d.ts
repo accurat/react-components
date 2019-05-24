6ed248496b90831c140d63b3384bb7a7771f878b
Marco Fugaro <marco.fugaro@gmail.com>
2019-05-24 16:08:00 +0200

    🔨  Fix declarations script

--- lib/lib/src/components/Toggle/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { InputPropsTypes, BooleanChangeFnType, ChangelessInputProps } from '../../commons/interfaces';
-interface ToggleProps extends InputPropsTypes, ChangelessInputProps {
-    onChange?: BooleanChangeFnType;
-}
-export default function Toggle({ children, className, inputClassName, style, disabled, checked, onChange, reset, }: ToggleProps): JSX.Element;
-export {};
--- lib/lib/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Button from './components/Button';
-import Checkbox from './components/Checkbox';
-import FlexView from './components/FlexView';
-import Radio from './components/Radio';
-import Select from './components/Select';
-import TextInput from './components/TextInput';
-import Toggle from './components/Toggle';
-export { Button, Checkbox, FlexView, Radio, Select, TextInput, Toggle };
