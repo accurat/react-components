8ba0429b64d7bba5ce09a49dd6bdff854a404d02
Luca Falasco <falasco.luca@gmail.com>
2019-07-31 16:26:27 +0200

    update docs

--- docs/lib/src/components/Radio/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { InputPropsTypes } from '../../commons/interfaces';
-export default function Radio({ children, className, inputClassName, style, disabled, checked, onChange, reset, ...props }: InputPropsTypes): JSX.Element;
--- docs/lib/src/components/Toggle/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { InputPropsTypes } from '../../commons/interfaces';
-export default function Toggle({ children, className, inputClassName, style, disabled, checked, onChange, reset, }: InputPropsTypes): JSX.Element;
